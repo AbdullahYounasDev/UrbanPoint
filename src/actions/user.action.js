fa69b47652cf559deb474dc63d1219251126aa9a
AbdullahYounasDev <ab.khokhar2005@gmail.com>
2024-11-04 15:50:16 +0500

    Adding filter in user action so that multiple docuuments of same user are not store in db

--- src/actions/user.action.js
+++ src/actions/user.action.js
@@ -5,7 +5,13 @@ import { NextResponse } from "next/server";
 export const createUser = async (user) => {
   try {
     await connect();
-
+    const existingUser = User.findOne({ clerkEmail: user.clerkEmail });
+    if (existingUser) {
+      return NextResponse.json(
+        { error: "User Already Exist" },
+        { user: existingUser },
+      );
+    }
     const newUser = await User.create(user);
     return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
