6b1aff0ac98991800f079f40ee28ac1167d2edcf
Abdullah Younas <149043115+AbdullahYounasDev@users.noreply.github.com>
2025-06-22 22:07:14 +0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import { Ubuntu } from "next/font/google";
 import "./globals.css";
 import React from "react";
 import { Provider } from "./nextauthprovider";
+import Script from "next/script";
 
 export const metadata = {
   title: "Property Rental Website",
@@ -20,6 +21,13 @@ export default function RootLayout({ children }) {
   return (
     <Provider>
       <html lang="en">
+    <head>
+          <Script
+            src="https://vozyo.vercel.app/widget.js"
+            data-chatbot="Ey3qJGsp"
+            strategy="afterInteractive"
+          />
+        </head>
         <body className={ubuntu.className}>{children}</body>
       </html>
     </Provider>
